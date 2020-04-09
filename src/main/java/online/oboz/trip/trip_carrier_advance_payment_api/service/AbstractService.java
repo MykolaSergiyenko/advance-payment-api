5d53f255cf07f9bb99d1e084139a6f577f24258a
vlas laptev <vlaptev@oboz.com>
2020-04-09 23:01:46 +0300

    delete unused code

--- src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/service/AbstractService.java
+++ src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/service/AbstractService.java
@@ -6,13 +6,6 @@ import org.springframework.http.MediaType;
 
 public abstract class AbstractService {
 
-    protected HttpEntity createHttpEntityWithAuthInfo(String body, String token) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", "Bearer " + token);
-        return new HttpEntity(body, headers);
-    }
-
     protected HttpEntity createHttpEntityToAuth(String body) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
