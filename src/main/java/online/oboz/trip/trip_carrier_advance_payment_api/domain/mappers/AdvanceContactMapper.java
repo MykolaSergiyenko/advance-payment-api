2277758e25fc2c291bcec1116a13655b07bc816b
nsergienko <Serg7778>
2020-07-21 12:30:20 +0300

    OBOZ-939 Не отображаются заказы в рабочем столе авансирования - Contact Mapper fix

--- src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/domain/mappers/AdvanceContactMapper.java
+++ src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/domain/mappers/AdvanceContactMapper.java
@@ -6,19 +6,22 @@ import online.oboz.trip.trip_carrier_advance_payment_api.web.api.dto.CarrierCont
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Mapper(componentModel = "spring", imports = {UUID.class, OffsetDateTime.class})
 public interface AdvanceContactMapper {
 
     AdvanceContactMapper contactMapper = Mappers.getMapper(AdvanceContactMapper.class);
 
-    @Mapping(source = "uuid", target = "uuid")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "uuid", expression = "java(UUID.randomUUID())")
+    @Mapping(target = "createdAt", expression = "java(OffsetDateTime.now())")
+    @Mapping(target = "updatedAt", expression = "java(OffsetDateTime.now())")
     @Mapping(source = "contractorId", target = "contractorId")
     @Mapping(source = "phoneNumber", target = "info.phone")
     @Mapping(source = "email", target = "info.email")
     @Mapping(source = "fullName", target = "info.fullName")
-    @Mapping(source = "createdAt", target = "createdAt")
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     AdvanceContactsBook toContactBook(CarrierContactDTO contactDTO);
 
 
