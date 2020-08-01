37e15ef69821d1b7b8900eba13ac9b41c1e0e56e
nsergienko <Serg7778>
2020-08-01 12:49:24 +0300

    Advance - front time-zone - fix

--- src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/service/util/DateUtils.java
+++ src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/service/util/DateUtils.java
@@ -1,11 +1,13 @@
 package online.oboz.trip.trip_carrier_advance_payment_api.service.util;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public interface DateUtils {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" E, dd.MM.yyyy в HH:mm:ss.SSS").
+        withZone(ZoneId.of("Europe/Moscow")).
         withLocale(new Locale("ru"));
 
     static String format(OffsetDateTime dateTime) {
