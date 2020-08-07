16663e2464b36b705805d6b55c7d83d90a88d5c4
nsergienko <Serg7778>
2020-08-07 20:29:26 +0300

    SecurityConfiguration - all endpoints with authorize (without 'carrier-page' and 'tests')

--- src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/config/SecurityConfiguration.java
+++ src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/config/SecurityConfiguration.java
@@ -25,21 +25,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+            //without aithorize
             .antMatchers(HttpMethod.GET, AUTH_WHITELIST).permitAll()
             .antMatchers(HttpMethod.POST, AUTH_WHITELIST).permitAll()
             .antMatchers(HttpMethod.GET, "/v1/advance_carrier/**").permitAll()
             .antMatchers(HttpMethod.POST, "/v1/advance_carrier/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/v1/trip_advance/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/v1/trip_advance/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/v1/advances/**").permitAll()
-            .antMatchers(HttpMethod.PUT, "/v1/advances/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/v1/advances/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/v1/advance_contacts/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/v1/advance_contacts/**").permitAll()
-            .antMatchers(HttpMethod.PUT, "/v1/advance_contacts/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/v1/advance_test/**").permitAll()
             .antMatchers(HttpMethod.POST, "/v1/advance_test/**").permitAll()
             .antMatchers(HttpMethod.PUT, "/v1/advance_test/**").permitAll()
+
+            // with authorize
+//            .antMatchers(HttpMethod.GET, "/v1/trip_advance/**").permitAll()
+//            .antMatchers(HttpMethod.POST, "/v1/trip_advance/**").permitAll()
+//            .antMatchers(HttpMethod.POST, "/v1/advances/**").permitAll()
+//            .antMatchers(HttpMethod.PUT, "/v1/advances/**").permitAll()
+//            .antMatchers(HttpMethod.GET, "/v1/advances/**").permitAll()
+//            .antMatchers(HttpMethod.GET, "/v1/advance_contacts/**").permitAll()
+//            .antMatchers(HttpMethod.POST, "/v1/advance_contacts/**").permitAll()
+//            .antMatchers(HttpMethod.PUT, "/v1/advance_contacts/**").permitAll()
+//            .antMatchers(HttpMethod.GET, "/v1/advance_test/**").permitAll()
+//            .antMatchers(HttpMethod.POST, "/v1/advance_test/**").permitAll()
+//            .antMatchers(HttpMethod.PUT, "/v1/advance_test/**").permitAll()
             .anyRequest().authenticated()
             .and().oauth2ResourceServer().jwt();
     }
