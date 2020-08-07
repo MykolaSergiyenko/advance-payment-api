a5ebf144cdb71b9574bfe1b3d1367eefba8d6f00
nsergienko <Serg7778>
2020-08-07 14:29:55 +0300

    Give auto-advances - trip-states, attachments query-fix

--- src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/repository/TripRepository.java
+++ src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/repository/TripRepository.java
@@ -14,9 +14,11 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
         "ac.isAutoContractor = true ) " +
         "where (t.tripStatusCode = 'assigned' and t.tripFields.tripTypeCode = 'motor') and " +
         "(t.tripCostInfo.cost between :minCost and :maxCost) and " +
-        "exists (select h from TripStateHistory h where h.toCode = 'assigned' and h.createdAt > :minDate) and " +
+        "exists (select h from TripStateHistory h where h.toCode = 'assigned' and h.createdAt > :minDate and " +
+        "h.tripId = t.id) and " +
         "exists (select atc from TripAttachment atc where (atc.documentTypeCode = 'trip_request' or " +
-        "atc.documentTypeCode = 'request') and atc.fileId is not null) and " +
+        "atc.documentTypeCode = 'request') and atc.fileId is not null and " +
+        "atc.tripId = t.id) and " +
         "not exists (select a from Advance a where t.id = a.advanceTripFields.tripId and " +
         "t.contractorId = a.contractorId and t.tripFields.driverId = a.advanceTripFields.driverId and " +
         "t.tripFields.orderId = a.advanceTripFields.orderId and t.tripFields.num = a.advanceTripFields.num)")
