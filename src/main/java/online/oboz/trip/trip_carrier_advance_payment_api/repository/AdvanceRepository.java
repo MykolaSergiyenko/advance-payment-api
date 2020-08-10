427db9ed12fdd7978f38aa225b3b2312dd7b2fb5
nsergienko <Serg7778>
2020-08-10 15:35:42 +0300

    is_cancelled to cancelled_at in AdvanceRepository - fix

--- src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/repository/AdvanceRepository.java
+++ src/main/java/online/oboz/trip/trip_carrier_advance_payment_api/repository/AdvanceRepository.java
@@ -62,8 +62,8 @@ public interface AdvanceRepository extends JpaRepository<Advance, Long> {
         value = "select * from orders.trip_request_advance_payment a inner join orders.trips t on " +
             "(t.id = a.trip_id and t.order_id = a.order_id and " +
             "t.driver_id = a.driver_id and t.contractor_id = a.contractor_id) " +
-            "where t.trip_status_code = 'assigned' and t.trip_type_code ='motor' and a.is_cancelled = false and " +
-            "a.read_at is null and a.sms_sent_at is null and a.email_sent_at is not null and " +
+            "where t.trip_status_code = 'assigned' and t.trip_type_code ='motor' and a.cancelled_at is null " +
+            "and a.read_at is null and a.sms_sent_at is null and a.email_sent_at is not null and " +
             "a.email_sent_at < (now() - (:minutes || ' minutes ') \\:\\:interval )")
     List<Advance> findUnreadAdvances(@Param("minutes") long minutes);
 
@@ -71,7 +71,7 @@ public interface AdvanceRepository extends JpaRepository<Advance, Long> {
     @Query(nativeQuery = true, value = "select (case when count(pc)> 0 then true else false end) " +
         "from orders.trip_request_advance_payment pc where (pc.trip_id = :trip_id and " +
         "pc.contractor_id = :contractor_id and pc.order_id = :order_id and pc.driver_id = :driver_id and " +
-        "pc.trip_num = :trip_num and pc.is_cancelled = false)")
+        "pc.trip_num = :trip_num and pc.cancelled_at is null)")
     Boolean existsActualByIds(@Param("trip_id") Long tripId, @Param("contractor_id") Long contractorId,
                               @Param("driver_id") Long driverId, @Param("order_id") Long orderId,
                               @Param("trip_num") String tripNum);
